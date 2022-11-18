6132867bfbbde803f6b744e1157a311529485dfc
Eduardo Rodriguez <edwardo16blak@gmail.com>
2022-11-18 11:48:10 -0800

    Environment setup and basics configurations for the project.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html, ts}'],
+  safelist: ['bg-blue-400', 'bg-green-400', 'bg-red-400'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
