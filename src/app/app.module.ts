f6df92db05e39bb3198947d444df23fb385c3540
Hillarys camilo <83847636+hillaryscamilo19@users.noreply.github.com>
2022-11-24 15:53:45 -0400

    Diseno de nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './componentes/nav/nav.component';
+
+//componentes de angular materia
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    FontAwesomeModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
